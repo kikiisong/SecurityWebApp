a2af99d415ae9e758b0e4fe360501ffb84bb32c9
kikiisong <55461623+kikiisong@users.noreply.github.com>
2021-12-01 22:58:37 -0500

    sum number of incidents by type

--- SecurityApp/src/main/java/model/IncidentManager.java
+++ SecurityApp/src/main/java/model/IncidentManager.java
@@ -2,7 +2,9 @@ package model;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class IncidentManager {
     static SimpleDateFormat inputDateFormatter = new SimpleDateFormat("yyyy-mm-dd");
@@ -16,5 +18,33 @@ public class IncidentManager {
         } catch (ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    public static int[] countIncidentsByType(List<Incident> incidents)
+    {
+        int[] result = new int[9];
+        for(Incident inc: incidents)
+        {
+            if(inc.getCrimeCode() == 1)
+                result[0]++;
+            else if(inc.getCrimeCode() == 2)
+                result[1]++;
+            else if(inc.getCrimeCode() == 3)
+                result[2]++;
+            else if(inc.getCrimeCode() == 4)
+                result[3]++;
+            else if(inc.getCrimeCode() == 5)
+                result[4]++;
+            else if(inc.getCrimeCode() == 6)
+                result[5]++;
+            else if(inc.getCrimeCode() == 7)
+                result[6]++;
+            else if(inc.getCrimeCode() == 8)
+                result[7]++;
+            else
+                result[8]++;
+        }
+
+        return result;
     }
 }
