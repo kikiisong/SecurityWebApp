52ea8b984832e7b6f29022c643a46b5667c5b794
gary2697 <zwu56@jh.edu>
2021-12-02 14:51:39 -0500

    load data

--- SecurityApp/src/main/resources/public/js/incidents-today.js
+++ SecurityApp/src/main/resources/public/js/incidents-today.js
@@ -1,6 +1,10 @@
+
+var data = [];
+
 function filterDate() {
-    var date = document.getElementById("date").value;
-    date = date.split('-').join('/');
+
+    var picked_day = document.getElementById("picked_day").value;
+    picked_day = picked_day.split('-').join('/');
 
     fetch('https://security-jhu-app.herokuapp.com/incidents-today?' + "&picked_day=" + picked_day, {
         method: 'POST',
@@ -29,6 +33,33 @@ function filterDate() {
     );
     console.log("FETCHED");*/
 }
+
+function loadVisData()
+{
+    var picked_day = document.getElementById("picked_day").value;
+    picked_day = picked_day.split('-').join('/');
+
+    fetch('https://security-jhu-app.herokuapp.com/incidents-today?'+ "&picked_day=" + picked_day ,{
+            method: 'GET',
+        }
+    ).then(
+        function(data){
+            var obj_list2 = document.getElementById("json2");
+            var content2 = obj_list2.innerHTML;
+            console.log(content2);
+            var json = JSON.parse(content2);
+            console.log(json);
+
+            for (var i = 0; i < json.length;i++)
+            {
+                console.log(json[i]) ;
+            }
+            // console.log(content2);
+            // console.log(data);
+        }
+    );
+}
+
 
 
 function visualization()
