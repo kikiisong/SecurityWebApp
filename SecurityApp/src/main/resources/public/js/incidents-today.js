5dd59039ced3efa2e4912a94bef500f92c07d059
gary2697 <zwu56@jh.edu>
2021-12-01 22:57:30 -0500

    get request for data

--- SecurityApp/src/main/resources/public/js/incidents-today.js
+++ SecurityApp/src/main/resources/public/js/incidents-today.js
@@ -1,31 +1,49 @@
 function filterDate() {
     const picked_day = document.getElementById("picked_day").value;
     console.log(picked_day);
-    $.ajax({
-        type: "GET",
-        url: "https://security-jhu-app.herokuapp.com/incidents-today?" + "&picked_day" + picked_day,
-        success: function (data) {
-            //We remove the old markers
+    fetch('https://security-jhu-app.herokuapp.com/incidents-today?'+ "&picked_day" + picked_day ,{
+            method: 'GET',
+        }
+    ).then(
+        function(data){
+            console.log(data);
             console.log(JSON.stringify(data));
-            // console.log(data);
-            // var jsonObj = $.parseJSON(data),
-            //     i;
+            //         // console.log(data);
+            //         var jsonObj = $.parseJSON(data),
+            //             i;
             var obj = JSON.stringify(data);
-            console.log("obj");
+            console.log(obj);
             var myobj = JSON.parse(obj);
             console.log(myobj);
-            // console.log(document.getElementById("id").innerHTML = myobj.longitude);
-            // beaches =[];//Erasing the beaches array
-
-            //Adding the new ones
-            // for(i=0;i < jsonObj.beaches.length; i++) {
-            //     beaches.push(jsonObj.beaches[i]);
-            // }
-
-            //Adding them to the map
-            // setMarkers(map, beaches);
         }
-    });
+    );
+    console.log("FETCHED");
+
+    // $.ajax({
+    //     type: "GET",
+    //     url: "https://security-jhu-app.herokuapp.com/incidents-today?" + "&picked_day" + picked_day,
+    //     success: function (data) {
+    //         //We remove the old markers
+    //         console.log(JSON.stringify(data));
+    //         // console.log(data);
+    //         // var jsonObj = $.parseJSON(data),
+    //         //     i;
+    //         var obj = JSON.stringify(data);
+    //         console.log("obj");
+    //         var myobj = JSON.parse(obj);
+    //         console.log(myobj);
+    //         // console.log(document.getElementById("id").innerHTML = myobj.longitude);
+    //         // beaches =[];//Erasing the beaches array
+    //
+    //         //Adding the new ones
+    //         // for(i=0;i < jsonObj.beaches.length; i++) {
+    //         //     beaches.push(jsonObj.beaches[i]);
+    //         // }
+    //
+    //         //Adding them to the map
+    //         // setMarkers(map, beaches);
+    //     }
+    // });
     console.log("test")
 }
 
