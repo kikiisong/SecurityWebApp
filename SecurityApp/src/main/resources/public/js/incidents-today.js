9b9b109ebe5002c1eafbcb53de1996047918f112
kikiisong <55461623+kikiisong@users.noreply.github.com>
2021-12-02 21:25:27 -0500

    bar chart with colors corresponding to markers

--- SecurityApp/src/main/resources/public/js/incidents-today.js
+++ SecurityApp/src/main/resources/public/js/incidents-today.js
@@ -75,11 +75,17 @@ function visualization()
                         parseInt(document.getElementById('type8').innerText),
                         parseInt(document.getElementById('type9').innerText)
                     ],
-                    lineTension: 0,
-                    backgroundColor: 'transparent',
-                    borderColor: '#007bff',
-                    borderWidth: 4,
-                    pointBackgroundColor: '#007bff'
+                    backgroundColor: [
+                        'rgb(54,162,235)',
+                        'rgb(169,227,119)',
+                        'rgb(220,119,227)',
+                        'rgb(139,119,227)',
+                        'rgb(215,167,95)',
+                        'rgb(231,110,173)',
+                        'rgb(164,164,164)',
+                        'rgb(232,236,169)',
+                        'rgb(175,251,255)'
+                    ]
                 }]
             },
             options: {
