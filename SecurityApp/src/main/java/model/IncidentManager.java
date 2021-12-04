7a3a9536dfca1e565ba2c6aff69322ad82f6d8ef
kikiisong <55461623+kikiisong@users.noreply.github.com>
2021-12-04 17:55:22 -0500

    default date=today

--- SecurityApp/src/main/java/model/IncidentManager.java
+++ SecurityApp/src/main/java/model/IncidentManager.java
@@ -1,8 +1,10 @@
 package model;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class IncidentManager {
-    public static String selectedDay = "2021/09/22";
+    public static String selectedDay = new SimpleDateFormat("yyyy/MM/dd").format(new Date());;
 
 
     public static int[] countIncidentsByType(List<Incident> incidents)
